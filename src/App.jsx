d29d4c9b953493f4972d18aa5a73d169a153d480
zacdev5 <zackorck@gmail.com>
2025-07-07 18:50:41 +0200

    implement dark and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
+import Toggle from "./components/Toggle";
 import { MovieProvider } from "./contexts/MovieContext";
+import { useState } from "react";
 
 function App() {
-  const movieNumber = 1;
+  const [isDark, setDarkMode] = useState(false);
 
   return (
     <MovieProvider>
-      <Navbar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/favorites" element={<Favorites />}/>
-        </Routes>
-      </main>
+      <div data-theme={isDark && "dark"}>
+        <Navbar />
+        <Toggle 
+          isChecked={isDark}
+          handleChange={() => setDarkMode(!isDark)}
+        />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/favorites" element={<Favorites />}/>
+          </Routes>
+        </main>
+      </div>
     </MovieProvider>
   );
 }
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,17 @@
+
+function Toggle({ handleChange, isChecked }) {
+  return (
+    <div className="toggle-container">
+      <input
+        type="checkbox"
+        id="check"
+        className="toggle"
+        onChange={handleChange}
+        checked={isChecked}
+      />
+      <label htmlFor="check">Dark Mode</label>
+    </div>
+  );
+}
+
+export default Toggle;
