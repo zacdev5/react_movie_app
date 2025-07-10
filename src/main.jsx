09168e0bf0d386d64e6ae09e48b3a90cf6bf941f
zacdev5 <zackorck@gmail.com>
2025-07-10 18:00:56 +0200

    fix site title and added basename to router component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_movie_app">
       <ThemeProvider>
         <App />
       </ThemeProvider>
